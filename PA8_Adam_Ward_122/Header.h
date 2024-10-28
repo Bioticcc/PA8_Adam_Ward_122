d3bce6b0fc11a4364b113cb1bd60ce15e8ccd27a
Bio <144750331+Bioticcc@users.noreply.github.com>
2024-10-28 13:55:13 -0700

    main
    tada

---
+++ PA8_Adam_Ward_122/Header.h
@@ -0,0 +1,33 @@
+/*
+Author: Adam Ward
+Date: 8/21/2024
+Description: PA8, some wacky binary search shenanigans ;3
+*/
+
+
+#pragma once
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>  
+#include <ostream>
+#include <string>      
+#include <vector>    
+#include <map>     
+#include <algorithm>   
+#include <stdexcept>  
+#include <fstream>
+#include <sstream>
+
+using std::ifstream;
+using std::ofstream;
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::endl;
+
+
+#endif 
---
+++ PA8_Adam_Ward_122/main.cpp
@@ -0,0 +1,20 @@
+#include "Header.h";
+
+int main() {
+
+	/*
+	- 2 BSTS, one representing products sold, one representing products purchased
+	- Okay, so when we begin you need to search for a few things:
+		- Abstraction in nodes?
+		- Inheritance in C++ classes?
+		- Public inheritance?
+		- Protected class data?
+		- virtual contstructors?
+		- pure virtual member functions?
+	- Abstract class Node
+	- Class TransactionNode witch inherits Node
+	- 
+	*/
+
+
+}
